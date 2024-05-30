5cf37a0b67cb9ceadf8504c254e84c791705bb08
angkul07 <angkul58@gmail.com>
2024-05-30 08:01:32 +0530

    rename the file and add the gui to browser

--- browser.py
+++ browser.py
@@ -1,5 +1,6 @@
 import socket
 import ssl
+import tkinter
 
 class URL:
     def __init__(self, url):
@@ -63,10 +64,27 @@ def show(body):
         elif not in_tag:
             print(c, end="")
 
-def load(url):
-    body = url.request()
-    show(body)
+
+class Browser:
+    def __init__(self):
+        WIDTH, HEIGHT = 800, 600
+        self.window = tkinter.Tk()
+        self.canvas = tkinter.Canvas(
+            self.window,
+            width=WIDTH,
+            height=HEIGHT
+        )
+        self.canvas.pack()
+
+    def load(self, url):
+        body = url.request()
+        self.canvas.create_rectangle(10, 20, 400, 300)
+        self.canvas.create_oval(100, 100, 150, 150)
+        self.canvas.create_text(200, 150, text="Hi!")
+        show(body)
+
 
 if __name__ == "__main__":
     import sys
-    load(URL(sys.argv[1]))
+    Browser().load(URL(sys.argv[1]))
+    tkinter.mainloop()
